f609db3871c49a2041cca284205dc97759000058
Juanito3500 <juanchavezsaldana1@gmail.com>
2024-03-20 21:58:21 -0500

    Proyects section cuadrada en pantalla

---
+++ src/assets/ProyectsData.ts
@@ -0,0 +1,19 @@
+import { ProyectProps } from "../components/Proyect/Proyect";
+
+export const proyects: ProyectProps[] = [
+  {
+    proyectName: "2O-SINTH",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis quas amet molestiae et alias ullam, unde veritatis quidem nulla doloremque iste laborum quos provident velit. Quos obcaecati nemo accusantium provident.",
+  },
+  {
+    proyectName: "TEMCHAT",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis quas amet molestiae et alias ullam, unde veritatis quidem nulla doloremque iste laborum quos provident velit. Quos obcaecati nemo accusantium provident.",
+  },
+  {
+    proyectName: "OPTIMIZER IO",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis quas amet molestiae et alias ullam, unde veritatis quidem nulla doloremque iste laborum quos provident velit. Quos obcaecati nemo accusantium provident.",
+  },
+];
